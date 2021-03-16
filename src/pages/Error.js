309001393e33e7f1c247fed2ade99f0b23a1ed88
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-16 14:42:36 +0100

    added: styled wrapper, div> h1 h3 Link from router

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <h2>Error!!!</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+`;
 
 export default Error;
