b1fc25283f0a1b2e1d3a3469df18a415a7287052
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-17 13:08:24 +0100

    added simple placeholder components

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Info, Navbar, Repos, Search, User } from "../components";
 
 const Dashboard = () => {
   return (
     <main>
       <h2>Dashboard</h2>
+      <Navbar />
+      <Repos />
+      <User />
+      <Search />
+      <Info />
     </main>
   );
 };
