e2254058d9e5855d403a1516b6d67cd483cd4b2a
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-17 13:44:37 +0100

    +created Context +Provider component

--- src/context/context.js
+++ src/context/context.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const url = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+//Provider & Consumer, i need only Provider thanks to useContext hook.
+
+const GithubProvider = ({ children }) => {
+  return <GithubContext.Provider value={}>{children}</GithubContext.Provider>;
+};
+
+export { GithubContext, GithubProvider };
