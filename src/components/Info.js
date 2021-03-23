d9d788810bd31b5fcae914a5d605532bb09a0c1e
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-23 16:20:57 +0100

    styled component +grid +color classes, +.item +span +@media for big screens

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,14 +6,15 @@ import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const Info = () => {
   const { githubUser } = React.useContext(GithubContext);
-  const { publick_repos, followers, following, publick_gists } = githubUser;
+  console.log(githubUser);
+  const { public_repos, followers, following, public_gists } = githubUser;
 
   const items = [
     {
       id: 1,
       icon: <GoRepo className="icon" />,
       label: "Repos",
-      value: publick_repos,
+      value: public_repos,
       color: "pink",
     },
     {
@@ -34,10 +35,13 @@ const Info = () => {
       id: 4,
       icon: <GoGist className="icon" />,
       label: "Gists",
-      value: publick_gists,
+      value: public_gists,
       color: "yellow",
     },
   ];
+
+  console.log(public_repos);
+
   return (
     <section className="section">
       <Wrapper className="section-center">
@@ -62,13 +66,57 @@ const Item = ({ icon, label, value, color }) => {
 };
 
 const Wrapper = styled.section`
-  .icon {
-    font-size: 1.5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 1rem 2rem;
+  @media (min-width: 640px) {
+    grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   }
+  .item {
+    border-radius: var(--radius);
+    padding: 1rem 2rem;
+    background: var(--clr-white);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    column-gap: 3rem;
+    align-items: center;
+    span {
+      width: 3rem;
+      height: 3rem;
+      display: grid;
+      place-items: center;
+      border-radius: 50%;
+    }
 
-  .pink {
-    background: #ffe0f0;
-    color: #da4a91;
+    .icon {
+      font-size: 1.5rem;
+    }
+
+    h3 {
+      margin-bottom: 0;
+      letter-spacing: 0;
+    }
+    p {
+      margin-bottom: 0;
+      text-transform: capitalize;
+    }
+
+    .pink {
+      background: #ffe0f0;
+      color: #da4a91;
+    }
+    .green {
+      background: var(--clr-primary-10);
+      color: var(--clr-primary-5);
+    }
+    .purple {
+      background: #e6e6ff;
+      color: #5d55fa;
+    }
+    .yellow {
+      background: #fffbea;
+      color: #f0b429;
+    }
   }
 `;
 
