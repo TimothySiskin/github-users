1e8a054ec22a013f258949f0e504d69f9160c4fd
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-24 17:55:30 +0100

    rendering data in component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { GithubContext } from "../context/context";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
 
 const Card = () => {
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser.mockUser;
   return (
-    <div>
-      <h3>Card</h3>
-    </div>
+    <>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || "no twitter"}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p>{bio}</p>
+      <div>
+        <p>
+          <MdBusiness /> {company || "no company"}
+        </p>
+        <p>
+          <MdLocationOn /> {location || "no location"}
+        </p>
+        <a href={`https://${blog}`}>
+          <MdLink></MdLink>
+          {blog || "no blog"}
+        </a>
+      </div>
+    </>
   );
 };
 
