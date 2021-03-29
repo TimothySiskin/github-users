6f01f3b682557a58ba57b6690d16362a8489d1b8
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-03-29 13:18:33 +0200

    +data from context + rendering data in component

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { GithubContext } from "../context/context";
 
 const Followers = () => {
+  const { followers } = React.useContext(GithubContext);
+  const followersArr = followers.mockFollowers;
+  console.log(followersArr);
   return (
-    <div>
-      <h2>Followers</h2>
+    <div className="followers">
+      {followersArr.map((follower, index) => {
+        const { avatar_url: img, html_url, login } = follower;
+        return (
+          <article key={index}>
+            <img src={img} alt={login} />
+            <div>
+              <h4>{login}</h4>
+            </div>
+            <a href={html_url}>{html_url}</a>
+          </article>
+        );
+      })}
     </div>
   );
 };
