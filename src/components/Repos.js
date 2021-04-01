66d973eb1aaeeb035bfc6b3d48f00550c8244d76
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-01 21:24:44 +0200

    +Context +styled component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,11 +1,24 @@
 import React from "react";
-
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
+  const { repos } = React.useContext(GithubContext);
+  const reposArr = repos.mockRepos;
+  console.log(reposArr);
   return (
-    <section>
-      <h2>Repos</h2>
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart />;
+      </Wrapper>
     </section>
   );
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-content: center;
+  gap: 2rem;
+`;
 
 export default Repos;
