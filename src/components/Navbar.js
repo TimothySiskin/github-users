68eb535013b0046f1037e0e491d4c87805fa0daf
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-12 12:35:41 +0200

    +styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <section>
+    <Wrapper>
       <h2>Navbar</h2>
-    </section>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.nav`
+  padding: 1.5rem;
+  margin-bottom: 4rem;
+  background: var(--clr-white);
+  text-align: center;
+  display: grid;
+  grid-template-columns: auto auto 100px;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
 
 export default Navbar;
