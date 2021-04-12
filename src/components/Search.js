73482156dbe6fa6cbe7c167601f07a9e5ebab34a
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-12 10:53:15 +0200

    +form +local state for user

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 
 const Search = () => {
+  //Local State
+  const [user, setUser] = useState("");
+  //Global Context
+  const handleSubmit = (e) => {
+    //placeholder code
+    console.log(e);
+  };
   return (
-    <section>
-      <h2>Search</h2>
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input type="text" placeholder="enter github user" />
+            <button type="submit">search</button>
+          </div>
+        </form>
+      </Wrapper>
     </section>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  gap: 1rem 1.75rem;
+`;
 
 export default Search;
