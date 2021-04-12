56d17c4ccef57e76aa7e7563f5cedc6476e6e4e9
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-04-12 11:34:25 +0200

    checkRequestst: +axios data fetch +requests state

--- src/context/context.js
+++ src/context/context.js
@@ -14,8 +14,33 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState({ mockUser });
   const [repos, setRepos] = useState({ mockRepos });
   const [followers, setFollowers] = useState({ mockFollowers });
+  //request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  //check rate
+  const checkRequests = () => {
+    axios(`${url}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          //throw an error
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //error
+
+  //SideEffects
+  useEffect(() => {
+    checkRequests();
+  }, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
